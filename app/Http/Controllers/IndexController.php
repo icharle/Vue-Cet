47c8dc52aec3eff668dda670be2a40cb345249a2
icharle <icharle@live.com>
2018-02-21 16:54:54 +0800

    抽象接口方法

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,40 +10,6 @@ use Whoops\Exception\ErrorException;
 
 class IndexController extends Controller
 {
-    /**
-     * @param $url
-     * @param $data
-     * @param $referer
-     * @return mixed
-     * CURL通用方法
-     */
-    public function curl($url, $type = 'get', $data = '', $referer)
-    {
-        //伪造Ip
-        $ip = mt_rand(1, 255) . "." . mt_rand(1, 255) . "." . mt_rand(1, 255) . "." . mt_rand(1, 255) . "";
-
-        //Curl处理
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("CLIENT-IP:" . $ip . "", "X_FORWARD_FOR:" . $ip . ""));
-        curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-        if ($referer != '') {
-            curl_setopt($curl, CURLOPT_REFERER, $referer);
-        }
-        if ($type = 'post') {
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        }
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $result = curl_exec($curl);
-        curl_close($curl);
-        return $result;
-    }
 
     /**
      * @return \Illuminate\Http\JsonResponse
@@ -51,7 +17,7 @@ class IndexController extends Controller
      */
     public function Tickets()
     {
-        $input = Input::all();
+        $input = Input::only('xm', 'sfz', 'jb');
         if (!isset($input['xm']) || empty($input['xm']) || !isset($input['sfz']) || empty($input['sfz']) || !isset($input['jb']) || empty($input['jb'])) {
             return response()
                 ->json([
@@ -59,19 +25,7 @@ class IndexController extends Controller
                     'msg' => '数据缺失,请完整输入个人信息！',
                 ]);
         } else {
-            $url = 'http://app.cet.edu.cn:7066/baas/app/setuser.do?method=UserVerify';
-            $referers = 'http://app.cet.edu.cn:7066/baas/app/setuser.do?method=UserVerify';
-            $data = array(
-                'ks_xm' => $input['xm'],
-                'ks_sfz' => $input['sfz'],
-                'jb' => $input['jb']
-            );
-            $post = array(
-                'action' => '',
-                'params' => json_encode($data)
-            );
-            $result = $this->curl($url, 'post', $post, $referers);
-            $res = json_decode($result, true);
+            $res = $this->GetTickets($input);
             if (isset($res['ks_bh'])) {
                 return response()
                     ->json([
@@ -99,7 +53,7 @@ class IndexController extends Controller
      */
     public function Score()
     {
-        $input = Input::all();
+        $input = Input::only('xm', 'zkz');
         if (!isset($input['xm']) || empty($input['xm']) || !isset($input['zkz']) || empty($input['zkz'])) {
             return response()
                 ->json([
@@ -107,27 +61,15 @@ class IndexController extends Controller
                     'msg' => '数据缺失,请完整输入个人信息！',
                 ]);
         } else {
-            $url = 'http://www.chsi.com.cn/cet/query?zkzh=' . $input['zkz'] . '&xm=' . urlencode($input['xm']);
-            $referers = 'http://www.chsi.com.cn/cet/';
-            $result = $this->curl($url, 'get', '', $referers);
-            preg_match_all('/<table[^>]+>(.*)<\/table>/isU', $result, $matches);
-            if (!isset($matches[0][1])) {
+            $cetScores = $this->GetScore($input);
+            if ($cetScores == false) {
                 return response()
                     ->json([
                         'status' => 500,
                         'msg' => '服务暂不可用！'
                     ]);
             }
-            preg_match_all('/(>)(.*?)(<)/s', $matches[0][1], $matches);
-            $search = ['<', '>', '：', chr(13) . chr(10)];
-            $replaceRes = str_replace($search, '', $matches[0]);
-            foreach ($replaceRes as $value) {
-                $arr[] = trim($value);
-            }
-            foreach (array_filter($arr) as $value) {
-                $cetScores[] = $value;
-            }
-            if (isset($cetScores[16])) {
+            else if (isset($cetScores[16])) {
                 return response()
                     ->json([
                         'status' => 200,
