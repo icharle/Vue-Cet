7c300befcb4093681dc00c3aa826ab82c5c5fa0c
icharle <icharle@live.com>
2018-02-25 02:44:28 +0800

    完成邮件通知模板

--- resources/views/Email/Score.blade.php
+++ resources/views/Email/Score.blade.php
@@ -1,27 +1,142 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>四六级成绩推送</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>四六级成绩单</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
-<body>
-<h1>成绩单</h1>
-<div>{{$data['name']}}</div>
+<body style="margin: 0; padding: 0;">
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+        <td style="padding: 10px 0 30px 0;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600"
+                   style="border: 1px solid #cccccc; border-collapse: collapse;">
+                <tr>
+                    <td align="center">
+                        <img src="{{ ('images/banner.jpg') }}" width="450" height="300"
+                             style="display: block;"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                                <td align="center"
+                                    style="color: #153643; font-family: Arial, sans-serif; font-size: 24px;">
+                                    <b>您的成绩单，请注意查收！</b><br><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                        <tbody>
+                                        <tr>
+                                            <th style="color: #153643; font-family: Arial, sans-serif; font-size: 18px;"
+                                                width="44%">姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名：
+                                            </th>
+                                            <td colspan="2">黄超
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th style="color: #153643; font-family: Arial, sans-serif; font-size: 18px;">
+                                                学&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;校：
+                                            </th>
+                                            <td colspan="2">华南理工大学广州学院
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th style="color: #153643; font-family: Arial, sans-serif; font-size: 18px;">
+                                                考试级别：
+                                            </th>
+                                            <td colspan="2">英语四级
+                                            </td>
+                                        </tr>
 
+                                        <tr>
+                                            <th bgcolor="#ee4c50" colspan="3" style="color: #ffffff">
+                                                笔试成绩
+                                            </th>
+                                        </tr>
 
-{{--name' => (string)$cetScores[3],      //姓名
-                    'school' => (string)$cetScores[7],     //学校
-                    'type' => (string)$cetScores[9],       //四级or六级
-                    'written' => [                                              //笔试部分
-                        'number' => (string)$cetScores[12],    //准考证号码
-                        'score' => (int)$cetScores[16],        //总分
-                        'listening' => (int)$cetScores[20],     //听力
-                        'reading' => (int)$cetScores[24],       //阅读
-                        'translation' => (int)$cetScores[26]        //翻译
-                    ],
-                    'oral' => [                                                 //口语部分
-                        'number' => (string)$cetScores[29],         //准考证号
-                        'score' => (string)$cetScores[33]           //等 级
-                    ]--}}
+                                        <tr>
+                                            <th style="color: #153643; font-family: Arial, sans-serif; font-size: 18px;">
+                                                准考证号：
+                                            </th>
+                                            <td colspan="2">445090171126123
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th style="color: #153643; font-family: Arial, sans-serif; font-size: 18px;">
+                                                总&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;分：
+                                            </th>
+                                            <td colspan="2">520</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td></td>
+                                            <th width="22%" align="left" style="color: #999999">
+                                                听&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;力：
+                                            </th>
+                                            <td>200</td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <th width="22%" align="left" style="color: #999999">
+                                                阅&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;读：
+                                            </th>
+                                            <td>160</td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <th width="22%" align="left" style="color: #999999">写作和翻译：
+                                            </th>
+                                            <td>160</td>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="3" bgcolor="#ee4c50" style="color: #ffffff">
+                                                口试成绩
+                                            </th>
+                                        </tr>
+                                        <tr>
+                                            <th style="color: #153643; font-family: Arial, sans-serif; font-size: 18px;">
+                                                准考证号：
+                                            </th>
+                                            <td colspan="2">--
+                                            </td>
+
+                                        </tr>
+                                        <tr>
+                                            <th style="color: #153643; font-family: Arial, sans-serif; font-size: 18px;">
+                                                等&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;级：
+                                            </th>
+                                            <td colspan="2">--
+                                            </td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#ee4c50" style="padding: 30px 30px 30px 30px;">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                                <td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;"
+                                    width="75%">
+                                    Copyright © 2018 <a href="https://icharle.com" style="color: #ffffff;"><font
+                                                color="#ffffff">Icharle</font></a>. All rights reserved.
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
 </body>
 </html>
+
